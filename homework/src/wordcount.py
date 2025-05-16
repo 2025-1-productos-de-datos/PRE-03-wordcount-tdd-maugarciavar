c52043b23ab745d4e97fca9f712d253983a0323c
Mauricio <maugarciavar@unal.edu.co>
2025-05-16 10:56:11 -0500

    CommitPRE-03Intento4

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -7,6 +7,7 @@ from homework.src._internals.read_all_lines import read_all_lines
 from homework.src._internals.count_words import count_words
 from homework.src._internals.preprocess_lines import preprocess_lines
 from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_word_counts
 import os
 
 def main():
@@ -18,3 +19,4 @@ def main():
     preprocessed_lines = preprocess_lines(lines)
     words = split_into_words(preprocessed_lines)
     word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)  # <-- Add this line
